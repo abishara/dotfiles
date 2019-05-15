991fd2b0471d84a35cf4db6445a5079c733cce7e
alex bishara <abishara@cs.stanford.edu>
2019-05-15 15:08:03 -0700

    symlink config files

---
+++ create_symlinks.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+GIT_ROOT=$(cd "$(dirname $0)"; pwd)
+CONFIG_ROOT=$GIT_ROOT/config
+BACKUP_ROOT=$HOME/.configbackups
+
+echo "Symlinking files from $CONFIG_ROOT to $HOME"
+
+for config_file in $(cd $CONFIG_ROOT && find . -type f | cut -c 3-)
+do
+    srcfile=$CONFIG_ROOT/$config_file
+    destfile=$HOME/$config_file
+    destdir=$(dirname $HOME/$config_file)
+    if [ -e $destfile -a ! -h $destfile ]; then
+        backupdir=$(dirname $BACKUP_ROOT/$config_file)
+        # Create directories; Don't just symlink directories back here.
+        mkdir -p $backupdir
+        mv $destfile $backupdir
+    fi
+    mkdir -p $destdir
+    ln -sf $CONFIG_ROOT/$config_file $destdir
+done
